bbd4486eb2e97b11f4df258ad1c46a09d59341d8
Region60 <_max_kot@mail.ru>
2022-09-09 00:25:27 +0300

    hrk

--- public/javascripts/services/dbServices.js
+++ public/javascripts/services/dbServices.js
@@ -1,7 +1,8 @@
 const { Pool } = require("pg")
 
 const pool = new Pool({
-  connectionString: 'postgres://wdnpqtloooqelx:8a64abf549a602ea31428aa73577a1d790e0fc86fea65bdd9e22bb3275210b55@ec2-54-87-179-4.compute-1.amazonaws.com:5432/dcajikbuhd7m6i'
+  connectionString: 'postgres://wdnpqtloooqelx:8a64abf549a602ea31428aa73577a1d790e0fc86fea65bdd9e22bb3275210b55@ec2-54-87-179-4.compute-1.amazonaws.com:5432/dcajikbuhd7m6i',
+ssl:false
 })
 
 
